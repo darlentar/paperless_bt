32f64c2f3173f447865ebc86df2dd7133b11579c
Damien Bihel <biheldamien@gmail.com>
2023-12-21 00:01:31 +0100

    add: compute_haversine

--- src/paperless_bt/coordinates.py
+++ src/paperless_bt/coordinates.py
@@ -1,4 +1,34 @@
+import math
+
 from pyproj import Transformer
+
+EARTH_RADIUS = 6371e3
+
+
+def compute_haversine(
+    latitude1: float,
+    latitude2: float,
+    longitude1: float,
+    longitude2: float,
+) -> float:
+    latitude1 = math.radians(latitude1)
+    latitude2 = math.radians(latitude2)
+    longitude1 = math.radians(longitude1)
+    longitude2 = math.radians(longitude2)
+    dlatidue = latitude2 - latitude1
+    dlongitude = longitude2 - longitude1
+    return (
+        2
+        * EARTH_RADIUS
+        * math.asin(
+            math.sqrt(
+                (math.sin(dlatidue / 2) ** 2)
+                + math.cos(latitude1)
+                * math.cos(latitude2)
+                * (math.sin(dlongitude / 2) ** 2)
+            )
+        )
+    )
 
 
 def lamber93_to_gps(x: int, y: int) -> tuple[int, int]:
--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -1,9 +1,32 @@
+import math
+
 import pytest
-from paperless_bt.coordinates import lamber93_to_gps
+from paperless_bt.coordinates import EARTH_RADIUS, compute_haversine, lamber93_to_gps
 
 
 def test_lamber93_to_gps():
     assert lamber93_to_gps(102980, 6847973) == (
         pytest.approx(-5.0888561),
         pytest.approx(48.4565745),
+    )
+
+
+@pytest.mark.parametrize(
+    "coordinates1, coordinates2, expected",
+    [
+        ((0, 0), (0, 0), 0),
+        ((0, 0), (90, 0), pytest.approx(math.pi / 2.0 * EARTH_RADIUS)),
+        ((0, 0), (-90, 23.12), pytest.approx(math.pi / 2.0 * EARTH_RADIUS)),
+        ((0, 0), (-45, 0), pytest.approx(math.pi / 4.0 * EARTH_RADIUS)),
+    ],
+)
+def test_haversine_formulat(coordinates1, coordinates2, expected):
+    assert (
+        compute_haversine(
+            coordinates1[0],
+            coordinates2[0],
+            coordinates1[1],
+            coordinates2[1],
+        )
+        == expected
     )
