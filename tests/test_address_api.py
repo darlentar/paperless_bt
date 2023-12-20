c8da14037db92262de0a0efe426ba4fe29ceb32a
Damien Bihel <biheldamien@gmail.com>
2023-12-20 22:11:16 +0100

    ignore: refactor (add fixture)

--- tests/test_address_api.py
+++ tests/test_address_api.py
@@ -1,3 +1,4 @@
+import pytest
 from paperless_bt.address_api import (
     Feature,
     Geometry,
@@ -6,10 +7,15 @@ from paperless_bt.address_api import (
 )
 
 
-def test_parse_address_api_response():
+@pytest.fixture
+def address_api_json() -> str:
     with open("address_api.json") as f:
         content = f.read()
-    assert parse_address_api_response(content).features[0] == Feature(
+    return content
+
+
+def test_parse_address_api_response(address_api_json):
+    assert parse_address_api_response(address_api_json).features[0] == Feature(
         geometry=Geometry(coordinates=[2.290084, 49.897442]),
         properties=Properties(
             label="8 Boulevard du Port 80000 Amiens",
